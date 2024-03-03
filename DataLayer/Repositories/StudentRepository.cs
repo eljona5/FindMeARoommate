3f880883536c98124e419fd75112ffc7aa9ee43b
eljona5 <eljonahila5@gmail.com>
2024-03-03 13:59:25 +0100

    Add Repository Student

---
+++ DataLayer/Repositories/StudentRepository.cs
@@ -0,0 +1,90 @@
+﻿using FindMeARoommate.DataLayer.DatabaseContext;
+using FindMeARoommate.DataLayer.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindMeARoommate.DataLayer.Repositories
+{
+    public class StudentRepository
+    {
+        public void AddStudent(Student student)
+        { // DbContext Add Method
+            var dbContext = new RoommateDBContext();
+            dbContext.Students.AddAsync(student);
+            dbContext.SaveChangesAsync();
+        }
+
+
+        // Get All Student
+        public List<Student> GetAllStudent(Student student)
+        {
+            var context = new RoommateDBContext();
+            var students = context.Students.ToList();
+
+            foreach (var s in students)
+            {
+                Console.WriteLine(s.Name + "         " + s.Surname + "        " + s.Address);
+            }
+             return students;
+        }
+        
+        // Get By ID
+
+       public  Student FindByID(int id)
+        {
+            try
+            {
+                var dbContext = new RoommateDBContext();
+
+                var student = dbContext.Students.Where(p => p.Id == id)
+                    .FirstOrDefault();
+
+                return student;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                throw ex;
+            }
+        }
+
+       
+        //Remove
+      public  void DeleteStudent(Student student)
+        {
+            try
+            {
+                var dbContext = new RoommateDBContext();
+                dbContext.Students.Remove(student);
+                dbContext.SaveChanges();
+                Console.WriteLine("Student removed");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error : " + ex.Message.ToString());
+                throw ex;
+            }
+        } 
+        
+        
+        //Update
+
+      public   void UpdateStudent(Student student)
+        {
+            try
+            {
+                var dbContext = new RoommateDBContext();
+                dbContext.Students.Update(student);
+                dbContext.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                throw ex;
+            }
+        }
+    }
+}
